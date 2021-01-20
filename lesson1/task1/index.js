fe2e4a924d1e9be656e34af3ce18b490b6bc6036
Volodymyr Meduna <liodik89@gmail.com>
2021-01-20 18:48:08 +0200

    first commit

---
+++ lesson1/task1/index.js
@@ -0,0 +1,5 @@
+const rootElem = document.querySelector('#root');
+
+const greetingElem = React.createElement('div', { className: 'greeting' }, 'Hello, React!');
+
+ReactDOM.render(greetingElem, rootElem);
---
+++ lesson1/task3/webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+
+    module: {
+      rules: [
+        {
+          test: /.js$/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /.css$/i,
+          use: [
+            'style-loader',
+            'css-loader',
+               ],
+        }
+      ]
+    },
+    
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      })
+    ],
+    devServer: {
+     
+      hot: true,
+    },
+  }
+  
