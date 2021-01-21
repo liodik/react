04078440a87a3a87e50adf16f6b43262ce96da58
Volodymyr Meduna <liodik89@gmail.com>
2021-01-21 10:38:29 +0200

    commit

--- lesson2/task1/webpack.config.js
+++ lesson2/task1/webpack.config.js
@@ -2,11 +2,13 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const webpack = require("webpack");
+const  path = require('path');
 
 module.exports = {
     entry: "./src/index.js",
     output: {
-        filename: "bundle.js"
+        path: path.resolve(__dirname, 'review_build'),
+    
     },
     module: {
         rules: [
