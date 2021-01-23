76b77552c87eab1edc64b0c045e82bdbdbbaed34
Volodymyr Meduna <liodik89@gmail.com>
2021-01-23 17:30:23 +0200

    commit

---
+++ lesson4/task2/src/Clock.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import './clock.scss'
+
+
+const getTimeWithOffset = offset => {
+    const currentTime = new Date();
+    const utcOffset = currentTime.getTimezoneOffset() / 60;
+return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+};
+
+
+class Clock extends Component {
+    constructor(props) {
+        super(props);
+        this.state= {
+            time: getTimeWithOffset(this.props.offset),
+        };
+    
+      
+    }
+    componentDidMount() {
+        this.timerID = setInterval(
+          () => this.tick(),
+          1000 );
+      }
+    tick() {
+        this.setState({
+          time: getTimeWithOffset(this.props.offset),
+        });
+      }
+
+         render() {
+        return (
+        <div className="clock">
+            <div className="clock__location">
+                {this.props.location}
+            </div>
+            <div className="clock__time">
+                {moment(this.state.time).format('LTS')}
+           </div>
+        </div>
+        );
+    }
+}
+
+
+export default Clock;
