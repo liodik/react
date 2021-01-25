fbc1f9670703d07e5ba574159d7dcb9df5369017
Volodymyr Meduna <liodik89@gmail.com>
2021-01-25 12:07:49 +0200

    commit

---
+++ lesson5/hw/task1/src/Toggler.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Toggler extends Component {
+  constructor() {
+    super();
+    this.state = {
+      buttonText: 'Off',
+    };
+  }
+  toggler = e => {
+    e.target.textContent === 'Off'
+      ? this.setState({ buttonText: 'Onn' })
+      : this.setState({ buttonText: 'Off' });
+  };
+
+  render() {
+    return (
+      <button className="toggler" onClick={this.toggler}>
+        {this.state.buttonText}
+      </button>
+    );
+  }
+}
+
+export default Toggler;
