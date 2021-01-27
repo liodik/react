3963c6481dd6b5a7682e2ea0d433db89272c4f8b
Volodymyr Meduna <liodik89@gmail.com>
2021-01-27 19:35:19 +0200

    commit

--- lesson8/task3/src/Dimension.jsx
+++ lesson8/task3/src/Dimension.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class Dimensions extends Component {
+class Dimension extends Component {
   state = {
     width: null,
     height: null,
@@ -35,4 +35,4 @@ class Dimensions extends Component {
   }
 }
 
-export default Dimensions;
+export default Dimension;
