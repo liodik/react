b53eced54d42cfd18c886418d9ad9dc8e36d0f31
Volodymyr Meduna <liodik89@gmail.com>
2021-01-27 18:31:57 +0200

    commit

---
+++ lesson8/task2/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Clock from './Clock';
+
+class App extends Component {
+  state = {
+    visible: true,
+  };
+  toggle = () => {
+    this.setState({
+      visible: !this.state.visible,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <button onClick={this.toggle}>Toggle</button>
+        <div>
+          {this.state.visible && (
+            <>
+              <Clock location={'London'} offset={0} />
+              <Clock location={'Kyiv'} offset={2} />
+              <Clock location={'New York'} offset={-5} />
+            </>
+          )}
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
