0e5e35bf67a4c074608e9ee478c9fb8aca6e3780
Volodymyr Meduna <liodik89@gmail.com>
2021-01-27 19:43:43 +0200

    commit

--- lesson8/task3/src/index.jsx
+++ lesson8/task3/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Dimensions from './Dimensions';
+import Dimension from './Dimensions';
 import './index.scss';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Dimensions />, rootElement);
+ReactDOM.render(<Dimension />, rootElement);
