44425065177be93bebd2a2adb9416f58e37b089e
Volodymyr Meduna <liodik89@gmail.com>
2021-01-29 15:29:04 +0200

    commit

--- lesson9/hw/task2/src/index .js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Game from './Game';
-import './index.scss';
-
-const rootElement = document.querySelector('#root');
-
-ReactDOM.render(<Game />, rootElement);
-export default Game;
