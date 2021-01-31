f176bb4c84836a61fc52f656bc5125c73b13b86e
Volodymyr Meduna <liodik89@gmail.com>
2021-01-31 17:26:09 +0200

    commit

--- lesson10/task2/src/App.jsx
+++ lesson10/task2/src/App.jsx
@@ -25,7 +25,7 @@ class App extends Component {
       <div className="page">
         <h1 className="title">{`Hello, ${this.state.userData.firstName} ${this.state.userData.lastName}`}</h1>
         <main className="content">
-          <ShoppingCart userData={this.state.userData} />
+          <ShoppingCart userName={this.state.userData.firstName} />
           <Profile userData={this.state.userData} handleChange={this.handleChange} />
         </main>
       </div>
