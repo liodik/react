d5ef529fd122dedab1f22b12040238315a4584ef
Volodymyr Meduna <liodik89@gmail.com>
2021-02-01 09:46:42 +0200

    commit

--- lesson10/task2/src/CartTitle.jsx
+++ lesson10/task2/src/CartTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CartTitle = ({ userName, count }) => {
   return (
-    <h3 className="cart-title">{`${userName.firstName} ${userName.lastName}, you added ${count} products`}</h3>
+    <h3 className="cart-title">{`${userName.firstName} ${userName.lastName}, you added ${count} items`}</h3>
   );
 };
 
