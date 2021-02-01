2353029dfabe67c215275c4a22a1704cf937c821
Volodymyr Meduna <liodik89@gmail.com>
2021-02-01 15:26:40 +0200

    commit

---
+++ lesson11/hw/task1/src/Expand.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Expand extends Component {
+  state = {
+    isOpen: false,
+  };
+  expandToggler = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+
+  render() {
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{this.props.title}</span>
+          <button className="expand__toggle-btn" onClick={this.expandToggler}>
+            {this.state.isOpen ? (
+              <i className="fas fa-chevron-up" />
+            ) : (
+              <i className="fas fa-chevron-down" />
+            )}
+          </button>
+        </div>
+        {this.state.isOpen && <div className="expand__content">{this.props.children}</div>}
+      </div>
+    );
+  }
+}
+
+Expand.propTypes = {
+  children: PropTypes.element.isRequired,
+  title: PropTypes.string,
+};
+Expand.defaultProps = {
+  title: '',
+};
+
+export default Expand;
