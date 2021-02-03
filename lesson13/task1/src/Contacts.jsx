767143ae9c862bafca96f0dc9b08764249fc55e7
Volodymyr Meduna <liodik89@gmail.com>
2021-02-03 10:13:49 +0200

    commit

---
+++ lesson13/task1/src/Contacts.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Contascts = () => {
+  return (
+    <div className="page__content">
+      <h1>Contacts</h1>
+      <p>We are here 🗺</p>
+      <Link to="/">Go home</Link>
+    </div>
+  );
+};
+export default Contascts;
