c766f890b118c94e9bbe714c153cd3a6ecdf5415
Volodymyr Meduna <liodik89@gmail.com>
2021-02-03 11:07:12 +0200

    commit

---
+++ lesson13/task2/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Products from './Products';
+
+const App = () => {
+  return (
+    <div className="Page">
+      <BrowserRouter>
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products" component={Products} />
+      </BrowserRouter>
+    </div>
+  );
+};
+export default App;
---
+++ lesson13/task2/src/Product.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Contascts = ({ match }) => {
+  return <div className="product">{`Product is a ${match.params.productId}`}</div>;
+};
+export default Contascts;
